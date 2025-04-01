10429d7f0fc103bf919b9a31eb7c390be1013115
maxrocks55 <mlhader@outlook.com>
2025-04-01 16:43:17 -0400

    Add files via upload

---
+++ website example/assets/js/jspage.js
@@ -0,0 +1,7 @@
+alert("this is javascript")
+var answer = prompt("are you okay with a quiz y/n")
+
+if ((answer = y) or (answer = Y)) {
+    var name = prompt("what's your name")
+    var age = parseInt(prompt("wha's your age"))
+}
