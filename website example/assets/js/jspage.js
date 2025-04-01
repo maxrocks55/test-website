6e97f35f3a79ddc63ffec186aefe3c12a9a66169
maxrocks55 <mlhader@outlook.com>
2025-04-01 16:44:08 -0400

    Delete website example directory

--- website example/assets/js/jspage.js
+++
@@ -1,7 +0,0 @@
-alert("this is javascript")
-var answer = prompt("are you okay with a quiz y/n")
-
-if ((answer = y) or (answer = Y)) {
-    var name = prompt("what's your name")
-    var age = parseInt(prompt("wha's your age"))
-}
